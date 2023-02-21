38d736514152abefa38f23391b0f1073ffedae22
AnupKamble <sahyadrihillsagro@gmail.com>
2023-02-21 18:01:31 +0530

    new app

---
+++ pdf-creater/src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import X from './X';
+
+function App() {
+  return (
+    <div className="App">
+     <X/>
+     <X/>
+     <X/>
+     <X/>
+     <X/>
+     
+     
+
+    </div>
+  );
+}
+
+export default App;
---
+++ pdf-creater/src/X.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function X() {
+  return (
+    <div>
+      <h1>MY NAME IS ANUP</h1>
+    </div>
+  )
+}
